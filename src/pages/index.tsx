7546641e8523655ea610565ba0371a59d9185c61
sasaki kaziu <kakazoo209@gmail.com>
2022-02-01 21:43:46 +0100

    add: add journey page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
     <div>
       Hello next.js
+      <Link href="/journey">journey</Link>
     </div>
   )
 }
---
+++ src/pages/journey/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next'
+
+const Index: NextPage = () => {
+  return (
+    <div>
+      This is journy page
+    </div>
+  )
+}
+
+export default Index
