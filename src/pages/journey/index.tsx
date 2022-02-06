b45b66f4fc0837d8db29715c19012b8425087ac5
sasaki kaziu <kakazoo209@gmail.com>
2022-02-06 13:59:57 +0100

    add: add news sentences and add style of world map pictures

--- src/pages/journey/index.tsx
+++ src/pages/journey/index.tsx
@@ -46,13 +46,117 @@ const Worldmap = styled.div`
   width: 100vw;
   height: 100vh;
   overflow: hidden;
+
+  // from here styles of three.js part
+  div[class^='desc-visible'] {
+    font-family: 'Quantico', sans-serif;
+    background: rgba(0, 0, 0, 0.7);
+    border-radius: 10px;
+    padding: 20px;
+    color: #00ffa7;
+    box-shadow: 0 4px 16px 0 rgba(#00ffa7, 0.4);
+    z-index: 1000;
+  }
+
+  div[class^='name-visible'] {
+    font-family: 'Segoe UI Black', sans-serif;
+    font-size: 50px;
+    font-weight: bold;
+    -webkit-background-clip: text;
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  //JSで指定すると上手くいかないので、とりあえずscssで指定
+  //japan
+  .name-visible0 {
+    background: linear-gradient(45deg, #e20112, #fff);
+  }
+
+  //spain
+  .name-visible1 {
+    background: linear-gradient(0deg, #e00011, #f8db09, #e00011);
+  }
+
+  //uk
+  .name-visible2 {
+    background: linear-gradient(0deg, #c3102d, #fff, #012066);
+  }
+
+  //germaney
+  .name-visible3 {
+    background: linear-gradient(0deg, #f8c800, #d70000, #000);
+  }
+
+  //poland
+  .name-visible4 {
+    background: linear-gradient(0deg, #e00011, #fff);
+  }
+
+  //ukraine
+  .name-visible5 {
+    background: linear-gradient(0deg, #ffd500, #52c3f1);
+  }
+
+  //mexico
+  .name-visible6 {
+    background: linear-gradient(0deg, #006847, #fff, #ce1126);
+  }
+
+  //guatemala
+  .name-visible7 {
+    background: linear-gradient(0deg, #4997d0, #fff, #4997d0);
+  }
+
+  //peru
+  .name-visible8 {
+    background: linear-gradient(0deg, #e60012, #fff, #e60012);
+  }
+
+  //bolivia
+  .name-visible9 {
+    background: linear-gradient(0deg, #e60012, #fff100, #009944);
+  }
+
+  //chile
+  .name-visible10 {
+    background: linear-gradient(0deg, #0055a2, #fff, #e60012);
+  }
+
+  //argentina
+  .name-visible11 {
+    background: linear-gradient(0deg, #74acdf, #fff, #74acdf);
+  }
+
+  //uruguay
+  .name-visible12 {
+    background: linear-gradient(0deg, #fff, #0038a8);
+  }
+
+  img[class^='pic-visible'] {
+    object-fit: cover;
+    border-radius: 50%;
+    filter: brightness(80%);
+    background: #1d2739;
+    border: white 5px solid;
+    box-shadow: 0 4px 16px 0 rgba(0, 255, 167, 0.4);
+  }
+
+  img[class^='flag-visible'] {
+    border-radius: 4px;
+    z-index: 1000;
+  }
+
+  .pic-text {
+    color: #fff;
+    z-index: 100;
+  }
 `
 const Canvas = styled.div<{
   openNewsModal: number | null
 }>`
   position: absolute;
   background-image: radial-gradient(#182b34, #2d1d34);
-  animation: .5s all ease-out;
   ${({ openNewsModal }) =>
     openNewsModal !== null &&
     css`
@@ -85,7 +189,7 @@ const DarkFullScreen = styled.div<{ openNewsModal: number | null }>`
   width: 0;
   height: 0;
   opacity: 0;
-  animation: 0.5s all ease-out;
+  transition: opacity 2s;
   ${({ openNewsModal }) =>
     openNewsModal !== null &&
     css`
@@ -93,6 +197,7 @@ const DarkFullScreen = styled.div<{ openNewsModal: number | null }>`
       width: 100%;
       height: 100%;
       opacity: 1;
+      transition: opacity 2s;
     `}  }
 `
 
--- src/pages/journey/newsModal.tsx
+++ src/pages/journey/newsModal.tsx
@@ -42,10 +42,15 @@ const FullScreen = styled.div<{ selectedNews: number | null }>`
   height: 100vh;
   width: 100vw;
   z-index: 1;
+  opacity: 1;
+  transition: 3s;
   ${({ selectedNews }) =>
     selectedNews === null &&
     css`
-      display: none;
+      height: 0;
+      width: 0;
+      opacity: 0;
+      transition: 3s;
     `}
 `
 const ModalWrapper = styled.div<{ selectedNews: number | null }>`
@@ -62,6 +67,23 @@ const ModalWrapper = styled.div<{ selectedNews: number | null }>`
   opacity: 0;
   font-weight: bold;
   transition: 0.7s opacity ease-out;
+  max-height: 500px;
+  overflow: scroll;
+
+  // TODO: these code need to move in other share file
+  ::-webkit-scrollbar {
+    width: 3px;
+    height: 0;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: #182b34;
+    border-radius: 5px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #1f443b;
+  }
   ${({ selectedNews }) =>
     selectedNews !== null &&
     css`
@@ -79,9 +101,12 @@ const CountryName = styled.div`
 `
 const Title = styled.div`
   font-size: 3rem;
+  margin-bottom: 1rem;
 `
 const Desc = styled.div`
   font-size: 1.5rem;
+  // to recognize new line code in json "¥¥n" in React.js
+  white-space: pre-line;
 `
 
 export default NewsModal
