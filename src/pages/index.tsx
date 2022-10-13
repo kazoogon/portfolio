d1700c557c949bfb01b387295ba1c146c89dd540
sasaki kaziu <kakazoo209@gmail.com>
2022-10-13 08:21:36 +0200

    add github logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,35 @@
 import type { NextPage } from 'next'
 import Index from './journey'
+import Github from '../assets/github.svg'
+import styled from 'styled-components'
 
 const Home: NextPage = () => {
   return (
     <>
       <Index />
       {/*<Link href="/journey">journey</Link>*/}
+      <IconWrapper>
+        <a
+          target="_blank"
+          href="https://github.com/kazoogon/portfolio"
+          rel="noreferrer"
+        >
+          <Github />
+        </a>
+      </IconWrapper>
     </>
   )
 }
 
+const IconWrapper = styled.div`
+  position: absolute;
+  right: 20px;
+  bottom: 10px;
+  cursor: pointer;
+
+  svg {
+    width: 30px;
+    height: 30px;
+  }
+`
 export default Home
